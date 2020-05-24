ee3cc04391b9c47ebdc71d9765d77aeeaafb0d9b
Juhasz Peter <b.juhaszpeter@gmail.com>
2020-05-24 17:56:08 +0200

    fix main

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const deck = require('./programfiles/deck');
+const bf = require('./programfiles/basicFunc');
 
 const main = () => {
   const bankHand = [];
   const playerHand = [];
-  const shuffledDeck = [];
+  const shuffledDeck = bf.shuffleDeck(deck.deckArray);
+  bf.mainHandDeal(shuffledDeck, playerHand, bankHand);
+  bf.revileTable(bankHand, playerHand);
+  bf.callPlayerCard(shuffledDeck, playerHand, bankHand, 4);
 };
 
 main();
--- programfiles/basicFunc.js
+++ programfiles/basicFunc.js
@@ -1,6 +1,7 @@
 const bankHand = [];
 const playerHand = [];
-const deck = require('./programfiles/deck');
+const deck = require('./deck');
+const readline = require('readline-sync');
 
 const shuffleDeck = (array) => {
   let currentIndex = array.length;
@@ -26,13 +27,39 @@ const revileCard = (handArray) => {
   console.log(handArray);
 };
 
+const revileTable = () => {
+  console.log('BANK:');
+  revileCard(bankHand[1]);
+  console.log(' ');
+  console.log('Player:');
+  revileCard(playerHand);
+};
+
+const mainHandDeal = (shuffledDeck, playerHand, bankHand) => {
+  let i = 0;
+  bankHand.push(shuffledDeck[i++]);
+  playerHand.push(shuffledDeck[i++]);
+  bankHand.push(shuffledDeck[i++]);
+  playerHand.push(shuffledDeck[i++]);
+};
+
+const callPlayerCard = (shuffledDeck, playerHand, num) => {
+  const call = readline.question('chose order:');
+
+  if (call === 'h') {
+    playerHand.push(shuffledDeck[num]);
+  } else if (call === 's') {
+    return playerHand;
+  }
+
+  revileTable();
+
+  callPlayerCard(shuffledDeck, playerHand, num + 1);
+};
 const shuffledDeck = shuffleDeck(deck.deckArray);
-dealCard(shuffledDeck, bankHand);
-console.log('BANK:');
-revileCard(bankHand);
-dealCard(shuffledDeck, playerHand);
-dealCard(shuffledDeck, bankHand);
-dealCard(shuffledDeck, playerHand);
-console.log(' ');
-console.log('Player:');
-revileCard(playerHand);
+
+mainHandDeal(shuffledDeck, playerHand, bankHand);
+
+revileTable();
+
+callPlayerCard(shuffledDeck, playerHand, 4);
